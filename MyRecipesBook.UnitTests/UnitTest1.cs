8ed35aeb07a682888bcf6fb4c26dec25729c4297
Gustavo Almeida <gustavo.almeida@live.com>
2022-01-17 23:44:28 -0300

    Add test project template

---
+++ MyRecipesBook.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MyRecipesBook.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
